2921fe04298bd6f0d12ba499e9465a5325d64ded
Geize Kele <ge.kele@hotmail.com>
2022-02-16 15:11:22 -0300

    atv04

---
+++ blastoff/exercicio04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempC;
+
+            Console.WriteLine("Digite a temperatura em Celsius: ");
+            tempC = Convert.ToDouble(Console.ReadLine());
+
+            var tempF = ((tempC * 9/5) + 32.0);
+
+            Console.WriteLine("A temperatura em Fahrenheit é {0}°F", tempF);
+        }
+    }
+}
