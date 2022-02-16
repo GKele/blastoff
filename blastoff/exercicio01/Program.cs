d4bfc3d2f3a41b2fce3797a38a2c1b5783eb10c4
Geize Kele <ge.kele@hotmail.com>
2022-02-16 14:34:18 -0300

    atv01 e atv02

---
+++ blastoff/exercicio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int I = 10;
+            int J = 20;
+            int K = 12;
+            int X = 9;
+            int Y = 20;
+
+            int soma = I + J + K + X + Y;
+            var media = (double)soma / 5;
+
+            Console.WriteLine("A média das idades é " + media);
+        }
+    }
+}
---
+++ blastoff/exercicio02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia = 450;
+            int combustivel = 32;
+
+            var consumo = (double) distancia / combustivel;
+
+            Console.WriteLine("Dada a distancia de " + distancia + "KM e o combustível de " + combustivel + "L, o consumo médio foi de {0}KM/L",consumo);
+        }
+    }
+}
