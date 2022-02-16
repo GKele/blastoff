1ac9bf713e4a6bafc534982bd144ec3023b85733
Geize Kele <ge.kele@hotmail.com>
2022-02-16 16:00:29 -0300

    atv09

---
+++ blastoff/exercicio09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 0; x <= 10; x++)
+            {
+                Console.WriteLine(numero + " * " + x + " = " + numero * x);
+            }
+        }
+    }
+}
