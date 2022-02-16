27ea9dd5c554c61df28a582b18f52fd59887c3d8
Geize Kele <ge.kele@hotmail.com>
2022-02-16 14:56:56 -0300

    atv03

---
+++ blastoff/exercicio03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Primeiro número: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Segundo número: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Terceiro número: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("{0} é o menor número", a);
+            }
+            if (b < c && b < a)
+            {
+                Console.WriteLine("{0} é o menor número", b);
+            }
+            if (c < a && c < b)
+            {
+                Console.WriteLine("{0} é o menor número", c);
+            }
+        }
+    }
+}
