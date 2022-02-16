b6ee088b1f8a9edeccc65c6f77937a8440bb78ea
Geize Kele <ge.kele@hotmail.com>
2022-02-16 15:35:56 -0300

    atv06

---
+++ blastoff/exercicio06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaA, horaB;
+
+            Console.WriteLine("Digite o horário de início da partida:");
+            horaA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o horário do fim da partida:");
+            horaB = Convert.ToInt32(Console.ReadLine());
+
+            var duracao = horaB - horaA;
+
+            Console.WriteLine("A duração da partidade foi de {0}h", duracao);
+
+        }
+    }
+}
