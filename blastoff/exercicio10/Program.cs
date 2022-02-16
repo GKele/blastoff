48fa1ae1c68fce459eda9cc34803df5fce8b1e8c
Geize Kele <ge.kele@hotmail.com>
2022-02-16 16:09:15 -0300

    atv10

---
+++ blastoff/exercicio10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i, numero, fatorial;
+            Console.WriteLine("Informe um número:");
+            numero = double.Parse(Console.ReadLine());
+
+            fatorial = numero;
+            for (i = numero - 1; i >= 1; i--)
+            {
+                Console.WriteLine($"{fatorial} * {i}");
+
+                fatorial = fatorial * i;
+
+                Console.WriteLine($"i={i}");
+                Console.WriteLine($"fatorial={fatorial}");
+                Console.WriteLine("  ");
+            }
+            Console.WriteLine("A fatorial de {0} é {1} ",numero, fatorial );
+        }
+    }
+}
