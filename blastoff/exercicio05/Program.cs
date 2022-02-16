5c2f726a56b40c62eb132891a4958d9f416c513a
Geize Kele <ge.kele@hotmail.com>
2022-02-16 15:20:30 -0300

    atv05

---
+++ blastoff/exercicio05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1%num2 == 0 || num2%num1 == 0)
+            {
+                Console.WriteLine("{0} e {1} são múltiplos", num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} e {1} não são múltiplos!", num1, num2);
+            }
+        }
+    }
+}
